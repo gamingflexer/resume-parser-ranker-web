64f9c645f37e04b69d1925de599d04cd0c84da4b
gamingflexer <osurve20ece@student.mes.ac.in>
2022-11-24 00:30:27 +0530

    auth api added

--- src/home/urls.py
+++ src/home/urls.py
@@ -1,6 +1,10 @@
 from . import views
 from django.urls import path
 
-urlpatterns = []
+urlpatterns = [     
+    #<--------- Authentication releted path / urls start here ------->
+    path('sign_up',views.Auth.sign_up,name='sign_up'),
+    path('sign_in',views.Auth.sign_in,name='sign_in'),
+    path('sign_out',views.Auth.sign_out,name='sign_out'),]
 
 #path('',views.rawData, name='rawData')
