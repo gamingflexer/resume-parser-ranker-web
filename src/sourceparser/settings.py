8929a84404d6d9583211662417bb4f2fcd03ec67
gamingflexer <osurve20ece@student.mes.ac.in>
2022-11-24 00:04:09 +0530

    settings updated | Env used for security

--- src/sourceparser/settings.py
+++ src/sourceparser/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
+
+#Keys
+SECRET_KEY = config('SECRET_KEY')
+MONGO_DB = config('MONGO_DB')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -18,9 +23,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ae!d+j^b86+8jvf(m^sn65)y2iauvxks-vqz)vso)#(oyz+mx6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +79,13 @@ WSGI_APPLICATION = 'sourceparser.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'djongo',
+        'NAME': 'sourceparser',
+        'ENFORCE_SCHEMA': False,
+        'CLIENT': {
+                'host': MONGO_DB,
+
+        }
     }
 }
 
