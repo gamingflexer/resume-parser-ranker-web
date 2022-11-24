89533036307a1882314c257bd756a7dfc736ebef
gamingflexer <osurve20ece@student.mes.ac.in>
2022-11-24 16:16:18 +0530

    urls updated

--- src/home/urls.py
+++ src/home/urls.py
@@ -3,9 +3,15 @@ from django.urls import path
 
 urlpatterns = [     
     #<--------- Authentication releted path / urls start here ------->
-    path('sign_up',views.Auth.sign_up,name='sign_up'),
-    path('sign_in',views.Auth.sign_in,name='sign_in'),
-    path('sign_out',views.Auth.sign_out,name='sign_out'),
+    path('signup',views.signup,name='signup'),
+    path('signin',views.signin,name='signin'),
+    path('signout',views.Auth.sign_out,name='sign_out'),
+    path('dashboard',views.dashboard,name='dashboard'),
+    path('upload',views.upload,name='upload'),
+    path('comingsoon',views.page_404,name='404'),
+    path('profile',views.profile,name='profile'),
+    path('forgetpassword',views.forget_password,name='forget_password'),
+    path('mail_recevied',views.mail_recevied,name='mail_recevied'),
     
     #<--------- other path / urls end here ------->
     path('', views.index, name='home'),
